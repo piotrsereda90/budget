728f50a5cfb6da3576c84b6d750aa7deb3d881bb
Piotr Sereda <piotrsereda90@gmail.com>
2021-07-15 13:55:11 +0200

    Add basic Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {Wrapper} from 'components';
+import {Link} from "react-router-dom";
+import { Container, List} from './Navigation.css';
+
+
+const Navigation =  ({items}) => {
+  return(
+    <Container>
+      <Wrapper>
+        <List>
+          {items.map(item => (
+            <li key={item.to}>
+              <Link to={item.to}>{item.content}</Link>
+            </li>
+          ))}
+
+        </List>
+      </Wrapper>
+    </Container>
+  )
+}
+export default Navigation
---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+width: 100%;
+margin: 0 auto;
+padding: 10px 0;
+max-width: 960px;
+`
+
+export default Wrapper;
